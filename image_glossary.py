b90ac05fa035e17729368744278c6183a7364bf7
Ralph Hempel <rhempel@hempeldesigngroup.com>
2024-09-01 15:17:49 -0400

    Added Don't Scale Your Teams article

--- image_glossary.py
+++ image_glossary.py
@@ -3,29 +3,35 @@ IMAGE_GLOSSARY = {
     { "uri" : "/images/accent/LLRV-pilot-kluever.thumbnail.jpg"
     , "title" : "Lunar Lander Research Vehicle Pilot - Kluever"
     , "alt" : "A picture of the NASA Lunar Research Vehice with the pilot (Kleuver) standing beside it"
-    , "source" : "" },
+    , "source" : "NASA Image Archive" },
 
   "Margaret Hamilton Code Stack":
     { "uri" : "/images/accent/MargaretHamiltonCodeStack.thumbnail.jpg"
     , "title" : "Margater Hamilton - Code Stack"
     , "alt" : "A picture of Margaret Hamilton standing next to a tall stack of printouts for the Apollo Lunar Lander"
-    , "source" : "" },
+    , "source" : "NASA Image Archive" },
 
  "Gene Kranz At Console":
    { "uri" : "/images/accent/GeneKranzAtConsole.thumbnail.jpg"
     , "title" : "Gene Kranz - At Console"
     , "alt" : "A picture of Gene Kranz looking intently at a console in NASA Mission Control"
-    , "source" : "" },
+    , "source" : "NASA Image Archive" },
 
  "Calatrava Arts and Sciences":
    { "uri" : "/images/accent/CalatravaArtsAndSciences.thumbnail.jpg"
     , "title" : "Valencia - City of Arts and Sciences"
     , "alt" : "The City of Arts and Sciences in Valencia - Santiago Calatrava"
-    , "source" : "" },
+    , "source" : "Ralph Hempel" },
 
  "HAL 9000 Front Panel":
    { "uri" : "/images/accent/Hal_9000_Panel.jpg"
     , "title" : "Hal-9000 Front Panel"
     , "alt" : "A picture of the Hal-9000 Front Panel From 2001: A Space Odyssey"
     , "source" : "https://en.wikipedia.org/wiki/HAL_9000" },
-}
+ 
+   "Wool Sheep":
+   { "uri" : "/images/accent/WoolSheep.jpg"
+    , "title" : "Wool Sheeep"
+    , "alt" : "Woolen sheep of various sizes in a wooden box with dividers"
+    , "source" : "Ralph Hempel" },
+ }
